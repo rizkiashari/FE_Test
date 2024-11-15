32b14959ef88ea681d09b8449c09ae699e51ef55
Ashari <80566147+rizkiashari@users.noreply.github.com>
2024-11-15 11:31:23 +0700

    feat: done logic login

--- src/layout/RootPage.jsx
+++ src/layout/RootPage.jsx
@@ -1,14 +1,23 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
 const RootPage = () => {
-  console.log("RootPage");
   const location = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
     // navigate("/login");
-  }, [navigate]);
+    const userLocalStorage = localStorage.getItem("dataUser");
+
+    if (userLocalStorage) {
+      // redirect to /pemindahan-barang
+      navigate("/pemindahan-barang");
+    } else {
+      navigate("/login");
+    }
+    // url / set to url /pemindahan-barang
+    if (location.pathname === "/") navigate("/pemindahan-barang");
+  }, [location.pathname, navigate]);
 
   return (
     <div className="font-roboto bg-[#F0F2F5] min-h-screen">
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,53 @@
+import { useState } from "react";
 import Button from "../components/atoms/Button";
 import TextInput from "../components/molecules/TextInput";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const onLogin = (e) => {
+    e.preventDefault();
+
+    console.log(data);
+    if (!data.username || !data.password) {
+      return Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Username and password must be filled!",
+      });
+    }
+
+    // add your login logic here
+    const dataLogin = JSON.stringify(data);
+    localStorage.setItem("dataUser", dataLogin);
+    navigate("/pemindahan-barang");
+  };
+
   return (
     <div className="flex h-screen items-center justify-center">
-      <form className="w-1/4 flex flex-col gap-4 justify-center">
-        <TextInput name="username" text="Username" placeholder="username" />
-        <TextInput name="password" text="Password" placeholder="password" />
+      <form
+        onSubmit={onLogin}
+        className="w-1/4 flex flex-col gap-4 justify-center"
+      >
+        <TextInput
+          name="username"
+          text="Username"
+          placeholder="username"
+          onChange={(e) => setData({ ...data, username: e.target.value })}
+        />
+        <TextInput
+          name="password"
+          text="Password"
+          placeholder="password"
+          onChange={(e) => setData({ ...data, password: e.target.value })}
+        />
         <Button
           text="Login"
           colorBg="#E5A000"
